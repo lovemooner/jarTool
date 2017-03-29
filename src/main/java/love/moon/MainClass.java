3e452a299a3f2a1b2b21b60a2a2236363f38c43c
lovemooner <lovemooner@yeah.net>
2017-03-29 10:50:32 +0800

    3

---
+++ src/main/java/love/moon/MainClass.java
@@ -0,0 +1,13 @@
+package love.moon;
+
+/**
+ * User: lovemooner
+ * Date: 17-3-29
+ * Time: 上午10:10
+ */
+public class MainClass {
+
+    public static void main(String[]args){
+        System.out.println("Hi");
+    }
+}
